e24d31bb6657f0e2f24b46a6ea0b303bf729bb7c
vxfontes <nessa1vane@icloud.com>
2022-10-15 08:17:06 -0300

    feat: adicionando avatares

---
+++ src/components/avatars/assustadoAvatar.tsx
@@ -0,0 +1,26 @@
+import { BigHead } from '@bigheads/core'
+
+const AssustadoAvatar = () => {
+    return (
+        <BigHead
+            accessory="none"
+            body="breasts"
+            circleColor="blue"
+            clothing="dress"
+            clothingColor="black"
+            eyebrows="raised"
+            eyes="dizzy"
+            facialHair="none"
+            graphic="vue"
+            hair="long"
+            hairColor="black"
+            hat="none"
+            hatColor="green"
+            lipColor="purple"
+            mouth="open"
+            skinTone="light"
+        />
+    );
+}
+
+export default AssustadoAvatar;
---
+++ src/components/avatars/normalAvatar.tsx
@@ -0,0 +1,26 @@
+import { BigHead } from '@bigheads/core'
+
+const NormalAvatar = () => {
+    return (
+        <BigHead
+            accessory="none"
+            body="breasts"
+            circleColor="blue"
+            clothing="dress"
+            clothingColor="black"
+            eyebrows="raised"
+            eyes="normal"
+            facialHair="none"
+            graphic="vue"
+            hair="long"
+            hairColor="black"
+            hat="none"
+            hatColor="green"
+            lipColor="purple"
+            mouth="openSmile"
+            skinTone="light"
+        />
+    );
+}
+
+export default NormalAvatar;
