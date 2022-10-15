105b96b6e0609dae9ff8add11f61dd8fe02c71c6
vxfontes <nessa1vane@icloud.com>
2022-10-15 01:19:42 -0300

    feat: criando arquivos da home e appbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import React from "react";
+import { ThemeProvider } from '@material-ui/styles';
+import theme from './theme';
+import Home from "./pages/home";
 
 const App = () => {
     return (
-        <>
-            <h1>oi</h1>
-        </>
+        <ThemeProvider theme={theme}>
+            <Home />
+        </ThemeProvider>
     );
 }
 export default App;
---
+++ src/pages/home/index.tsx
@@ -0,0 +1,12 @@
+import MyAppBar from "../../components/appBar"
+
+
+const Home = () => {
+    return (
+        <>
+            <MyAppBar />
+        </>
+    );
+}
+
+export default Home;
