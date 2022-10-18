4b265b754f8a8d29d7cda6a9e536fff1731cc77e
vxfontes <nessa1vane@icloud.com>
2022-10-17 22:06:37 -0300

    adicionando rotas

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
-import React from "react";
 import { ThemeProvider } from '@material-ui/styles';
 import theme from './theme';
-import Home from "./pages/home";
+import RoutesApp from "./routes";
 
 const App = () => {
     return (
             <ThemeProvider theme={theme}>
-                <Home />
+                <RoutesApp />
             </ThemeProvider>
     );
 }
