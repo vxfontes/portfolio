0444dd181425121efa2d2023e879588fadc41324
vxfontes <nessa1vane@icloud.com>
2022-10-18 11:43:34 -0300

    fix: continuando apresentação

--- src/pages/presentation/index.tsx
+++ src/pages/presentation/index.tsx
@@ -1,12 +1,21 @@
-import { Grid } from '@material-ui/core';
+import { Button, Grid, Typography } from '@material-ui/core';
+import NormalAvatar from '../../components/avatars/normalAvatar';
 import styles from './styles';
 
 const Presentation = () => {
     const classes = styles();
 
     return (
-        <Grid className={classes.paper} container>
-            <h1>hi</h1>
+        <Grid className={classes.paper} container direction="row" justifyContent="center" alignItems="center">
+            <Grid item xs={12} sm={6}>
+                <NormalAvatar />
+            </Grid>
+            <Grid item xs={12} sm={6}>
+                <Button variant="outlined">Default</Button>
+                <Button variant="contained" color="primary">
+                    Primary
+                </Button>
+            </Grid>
         </Grid>
     );
 }
