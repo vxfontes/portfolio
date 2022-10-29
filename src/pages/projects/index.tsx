db2dd83ea83b11fb048558b3316d4a500a8e113d
vxfontes <nessa1vane@icloud.com>
2022-10-29 10:03:03 -0300

    feat: textura e primeiro uso do carousel

--- src/pages/projects/index.tsx
+++ src/pages/projects/index.tsx
@@ -1,10 +1,36 @@
-import { Grid, Typography } from "@material-ui/core";
+import { Button, Grid, Paper, Typography } from "@material-ui/core";
+import Carousel from "react-material-ui-carousel";
 import { LanguageProps } from "../../interfaces/languageProps";
 import styles from "./styles";
 
 const Projects = (get: LanguageProps) => {
     const language = get.language;
     const classes = styles();
+
+    var items = [
+        {
+            name: "Random Name #1",
+            description: "Probably the most random thing you have ever seen!"
+        },
+        {
+            name: "Random Name #2",
+            description: "Hello World!"
+        }
+    ]
+
+    function Item(props: any) {
+        return (
+            <Paper>
+                <h2>{props.item.name}</h2>
+                <p>{props.item.description}</p>
+
+                <Button className="CheckButton">
+                    Check it out!
+                </Button>
+            </Paper>
+        )
+    }
+
 
     return (
         <>
@@ -15,7 +41,13 @@ const Projects = (get: LanguageProps) => {
             )}; */}
 
             <Grid className={classes.paper} container direction='row' justifyContent='center' alignItems="center" spacing={1}>
-                <Typography variant='h2'>Projetos</Typography>
+                <Typography variant='h2' color='secondary'>Projetos</Typography>
+                <br />
+                <Carousel>
+                    {
+                        items.map((item, i) => <Item key={i} item={item} />)
+                    }
+                </Carousel>
             </Grid>
         </>
     )
