76198d6dbbf8b4f1c3b7fe2ca5b091eb7e6a7c19
vxfontes <nessa1vane@icloud.com>
2022-10-29 09:15:44 -0300

    feat: adicionando carousel a packages

--- src/pages/projects/index.tsx
+++ src/pages/projects/index.tsx
@@ -1,3 +1,4 @@
+import { Grid, Typography } from "@material-ui/core";
 import { LanguageProps } from "../../interfaces/languageProps";
 import styles from "./styles";
 
@@ -7,11 +8,15 @@ const Projects = (get: LanguageProps) => {
 
     return (
         <>
-            {language === 'english' ? (
+            {/* {language === 'english' ? (
                 <h1>ok</h1>
             ) : (
                 <h1>ok</h1>
-            )};
+            )}; */}
+
+            <Grid className={classes.paper} container direction='row' justifyContent='center' alignItems="center" spacing={1}>
+                <Typography variant='h2'>Projetos</Typography>
+            </Grid>
         </>
     )
 }
--- src/pages/projects/styles.ts
+++ src/pages/projects/styles.ts
@@ -2,6 +2,14 @@ import { makeStyles, createStyles, Theme } from '@material-ui/core';
 
 const styles = makeStyles((theme: Theme) =>
     createStyles({
+        paper: {
+            width: '100%',
+            height: '100vh',
+            margin: 0,
+            padding: 0,
+            backgroundColor: theme.palette.mybackgroundColor?.main,
+            textAlign: 'center',
+        },
 
     })
 );
