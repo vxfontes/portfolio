02cbe2670a3c1ec347217b8ba56135c81859d974
vxfontes <nessa1vane@icloud.com>
2022-11-09 15:27:21 -0300

    fix: key

--- src/utils/firebaseKey.ts
+++
@@ -1,9 +0,0 @@
-export const firebaseConfig = {
-    apiKey: "AIzaSyBC0-YARy4uVOVqYILB5Tm8tcuwRvMS6FE",
-    authDomain: "vxfontes.firebaseapp.com",
-    projectId: "vxfontes",
-    storageBucket: "vxfontes.appspot.com",
-    messagingSenderId: "220100464195",
-    appId: "1:220100464195:web:621a10d98be1d5a8ca0ef6",
-    measurementId: "G-S1KPQ7HK67"
-};
