7ff2047b4fd3f8dde50b99a1ddb6e258a6c9a040
vxfontes <nessa1vane@icloud.com>
2022-11-18 11:17:32 -0300

    feat: inicio da pagina e transições

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
 import { ThemeProvider } from '@material-ui/styles';
 import theme from './theme';
 import RoutesApp from "./routes";
+import { BrowserRouter as Router, Route, Routes, useLocation } from 'react-router-dom';
+import ScrollToTop from './components/scroll';
 
 const App = () => {
     return (
         <ThemeProvider theme={theme}>
-            <RoutesApp />
+            <Router>
+                <ScrollToTop />
+                <RoutesApp />
+            </Router>
         </ThemeProvider>
     );
 }
--- src/routes.tsx
+++ src/routes.tsx
@@ -1,34 +1,41 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, useLocation } from 'react-router-dom';
+import { TransitionGroup, CSSTransition } from 'react-transition-group';
+import './main.css'
+
+// pages
 import About from './pages/about';
 import Home from './pages/home';
 import CallingPages from './pages/home/callingPages';
-import Presentation from './pages/presentation';
 import Infos from './pages/projects/infos';
 import ProjectsPage from './pages/projects/projectsPage';
 import Skills from './pages/skills';
 import TestBreakpoint from './test/breakpoints';
 
 const RoutesApp = () => {
+    const location = useLocation();
+
     return (
-        <Router>
-            <Routes>
-                <Route path='/' element={<Home />}/>
-                <Route path='/:language' element={<CallingPages />}/>
-                <Route path='/breakpoints' element={<TestBreakpoint />}/>
+        <TransitionGroup>
+            <CSSTransition timeout={250} classNames='fade' key={location.key}>
+                <Routes location={location}>
+                    <Route path='/' element={<Home />} />
+                    <Route path='/:language' element={<CallingPages />} />
+                    <Route path='/breakpoints' element={<TestBreakpoint />} />
 
-                {/* projects */}
-                <Route path='/projectsPage/:language' element={<ProjectsPage />}/>
-                <Route path='/project/:language/:project' element={<Infos />}/>
-                
-                {/* ingles */}
-                <Route path='/about/:language' element={<About />}/>
-                <Route path='/skills/:language' element={<Skills />}/>
+                    {/* projects */}
+                    <Route path='/projectsPage/:language' element={<ProjectsPage />} />
+                    <Route path='/project/:language/:project' element={<Infos />} />
 
-                {/* portugues */}
-                <Route path='/sobre/:language' element={<About />}/>
-                <Route path='/habilidades/:language' element={<Skills />}/>
-            </Routes>
-        </Router>
+                    {/* ingles */}
+                    <Route path='/about/:language' element={<About />} />
+                    <Route path='/skills/:language' element={<Skills />} />
+
+                    {/* portugues */}
+                    <Route path='/sobre/:language' element={<About />} />
+                    <Route path='/habilidades/:language' element={<Skills />} />
+                </Routes>
+            </CSSTransition>
+        </TransitionGroup>
     );
 }
 
