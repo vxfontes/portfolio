eadd252c87a294af55cac40ab029411abcd9ab4b
vxfontes <nessa1vane@icloud.com>
2022-11-26 18:14:33 -0300

    fix: arrumando link do curriculo

--- src/pages/presentation/index.tsx
+++ src/pages/presentation/index.tsx
@@ -1,5 +1,6 @@
 import { Button, Grid, Typography, Link } from '@material-ui/core';
 import { ArrowForward } from '@material-ui/icons';
+import { useState } from 'react';
 import { useParams } from 'react-router-dom';
 
 import NormalAvatar from '../../components/avatars/normalAvatar';
@@ -7,7 +8,7 @@ import styles from './styles';
 
 
 const Presentation = () => {
-    
+
     const params = useParams();
     const language = params.language;
     const classes = styles();
@@ -36,33 +37,36 @@ const Presentation = () => {
                     <Grid item xl={8} lg={7} md={9} sm={12} xs={12}>
                         <Grid container className={classes.justify} direction='row' spacing={1}>
                             <Grid item xl={6} lg={7} md={7} sm={10} xs={11}>
-                                <Link style={{ textDecoration: 'none', color: 'white' }} href='https://drive.google.com/file/d/1gM8mLgrf04lyBxHpG40om1kM2iQSoTmz/view?usp=sharing'>
-                                    <Button className={classes.typo} variant="outlined" color='secondary' fullWidth>
-                                        {language === 'portuguese' ? (
+                                {language === 'portuguese' ? (
+                                    <Link style={{ textDecoration: 'none', color: 'white' }} href={'https://drive.google.com/file/d/1KGFsFH7r0WEqJk_V3iRwjv3hJdwaXIg6/view?usp=sharing'}>
+                                        <Button className={classes.typo} variant="outlined" color='secondary' fullWidth>
                                             <Typography variant='h6'>
-                                                Baixar currículo
+                                                <>Baixar currículo</>
                                             </Typography>
-                                        ) : (
+                                            <ArrowForward style={{ marginLeft: '1vw' }} />
+                                        </Button>
+                                    </Link>
+                                ) : (
+                                    <Link style={{ textDecoration: 'none', color: 'white' }} href={'https://drive.google.com/file/d/1Xf3j0PX6t2cR6t1I4FiH3WDo_KCEJ2VZ/view?usp=sharing'}>
+                                        <Button className={classes.typo} variant="outlined" color='secondary' fullWidth>
                                             <Typography variant='h6'>
-                                                Download Resume
+                                                <>Baixar currículo</>
                                             </Typography>
-                                        )}
-                                        <ArrowForward style={{ marginLeft: '1vw' }} />
-                                    </Button>
-                                </Link>
+                                            <ArrowForward style={{ marginLeft: '1vw' }} />
+                                        </Button>
+                                    </Link>
+                                )}
                             </Grid>
                             <Grid item xl={4} lg={4} md={4} sm={10} xs={11}>
                                 <Link href='https://api.whatsapp.com/send?phone=5575988389894' style={{ textDecoration: 'none', color: 'white' }}>
                                     <Button className={classes.typo} variant="contained" color="primary" fullWidth>
-                                        {language === 'portuguese' ? (
-                                            <Typography variant='h6'>
-                                                Me Contate
-                                            </Typography>
-                                        ) : (
-                                            <Typography variant='h6'>
-                                                Contact Me
-                                            </Typography>
-                                        )}
+                                        <Typography variant='h6'>
+                                            {language === 'portuguese' ? (
+                                                <>Me Contate</>
+                                            ) : (
+                                                <>Contact Me</>
+                                            )}
+                                        </Typography>
                                     </Button>
                                 </Link>
                             </Grid>
