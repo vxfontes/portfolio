e1eca8d7a767469abe023a762877cd73d69c5944
Vanessa <nessa1vane@icloud.com>
2023-08-04 17:02:46 -0300

    feat: nova versão agora com nextjs

--- src/firebase.ts
+++
@@ -1,8 +0,0 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import firebaseConfig from './utils/firebaseKey';
-import { getFirestore } from "firebase/firestore";
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-export const dataBaseApp = getFirestore(app);
