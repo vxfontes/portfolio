70ff2569576ba8d278b086f7844a538384041ca0
Vanessa <nessa1vane@icloud.com>
2023-08-14 21:23:36 -0300

    feat: adicionando experiencia

---
+++ src/data/experiences.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { CgWorkAlt } from "react-icons/cg";
+import { FaReact } from "react-icons/fa";
+import { LuGraduationCap } from "react-icons/lu";
+
+export const experiencesData = [
+    {
+        title: "Graduated bootcamp",
+        location: "Miami, FL",
+        description:
+            "I graduated after 6 months of studying. I immediately found a job as a front-end developer.",
+        icon: <LuGraduationCap />,
+        date: "2019",
+    },
+    {
+        title: "Front-End Developer",
+        location: "Orlando, FL",
+        description:
+            "I worked as a front-end developer for 2 years in 1 job and 1 year in another job. I also upskilled to the full stack.",
+        icon: <CgWorkAlt />,
+        date: "2019 - 2021",
+    },
+    {
+        title: "Full-Stack Developer",
+        location: "Houston, TX",
+        description:
+            "I'm now a full-stack developer working as a freelancer. My stack includes React, Next.js, TypeScript, Tailwind, Prisma and MongoDB",
+        icon: <FaReact />,
+        date: "2021 - present",
+    },
+] as const;
