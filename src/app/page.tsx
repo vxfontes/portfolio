f9ce61d027687bee0d725c84f8ff4c0c8dff1a32
vxfontes <nessa1vane@icloud.com>
2023-08-22 14:51:30 -0300

    feat: primeira tela de projetos

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ import About from "@/components/About";
 import Experience from "@/components/Experience";
 import Header from "@/components/Header";
 import Intro from "@/components/Intro";
+import Projects from "@/components/Projects";
 import Skills from "@/components/Skills";
 
 export default function Home() {
@@ -24,8 +25,12 @@ export default function Home() {
                 <Experience />
             </section>
 
-            <section id="skills" className="snap-center">
+            <section id="skills" className="snap-start">
                 <Skills />
+            </section>
+
+            <section id="projects" className="snap-start">
+                <Projects />
             </section>
         </main>
     )
