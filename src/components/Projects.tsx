c49c1afdf2f85031175a45702a9fc5003cb20e16
vxfontes <nessa1vane@icloud.com>
2023-08-28 12:17:49 -0300

    feat: nova tela de projetos

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,46 +1,78 @@
-import { motion } from "framer-motion";
+import { motion, useScroll, useTransform } from "framer-motion";
 import Title from "./Title";
+import Image from "next/image";
+import { projects } from "@/data/projects";
 
 const Projects = () => {
 
-    const projects = [1, 2, 3, 4, 5]
+    const { scrollYProgress } = useScroll({
+        offset: ["0 1", "1.33 1"],
+    });
+    const scaleProgess = useTransform(scrollYProgress, [0, 1], [0.8, 1]);
+    const opacityProgess = useTransform(scrollYProgress, [0, 1], [0.6, 1]);
+
 
     return (
         <motion.div
             initial={{ opacity: 0 }}
             whileInView={{ opacity: 1 }}
             transition={{ duration: 1.5 }}
-            className="h-screen relative flex overflow-hidden flex-col text-left md:flex-row max-w-full justify-evenly mx-auto items-center z-0"
+            className="relative flex overflow-hidden flex-col text-left max-w-full justify-evenly mx-auto items-center z-0"
         >
             <Title>Projects</Title>
 
-            <div className="relative w-full flex overflow-x-scroll overflow-y-hidden snap-x snap-mandatory z-20">
+            <div className="columns-1 md:columns-2 gap-2 px-16 pt-40 w-full overflow-x-scroll overflow-y-hidden snap-x snap-mandatory z-20">
                 {projects.map((project, index) => (
-                    <div key={project} className="w-screen flex-shrink-0 snap-center flex flex-col space-y-5 items-center justify-center p-20 md:p-44 h-screen">
-                        <motion.img
-                            initial={{
-                                y: -300,
-                                opacity: 0
-                            }}
-                            transition={{ duration: 1.2 }}
-                            whileInView={{ opacity: 1, y: 0 }}
-                            viewport={{ once: true }}
-                            src="https://img.freepik.com/icones-gratis/twitter_318-177987.jpg"
-                            alt=""
-                        />
+                    <motion.div
+                        key={index}
+                        initial={{
+                            y: -300,
+                            opacity: 0
+                        }}
+                        transition={{ duration: 1.2 }}
+                        whileInView={{ opacity: 1, y: 0 }}
+                        viewport={{ once: true }}
+                        style={{
+                            scale: scaleProgess,
+                            opacity: opacityProgess,
+                        }}
+                        className="group mb-3 sm:mb-8 last:mb-0"
+                    >
+                        <section className="bg-gray-100 max-w-[42rem] border border-black/5 rounded-lg overflow-hidden sm:pr-8 relative sm:h-[20rem] hover:bg-gray-200 transition sm:group-even:pl-8 dark:text-white dark:bg-white/10 dark:hover:bg-white/20">
+                            <div className="pt-4 pb-7 px-5 sm:pl-10 sm:pr-2 sm:pt-10 sm:max-w-[50%] flex flex-col h-full sm:group-even:ml-[18rem]">
+                                <h3 className="text-2xl font-semibold">{project.title}</h3>
+                                <p className="mt-2 leading-relaxed text-gray-700 dark:text-white/70">
+                                    {project.descriptionPT}
+                                </p>
+                                <ul className="flex flex-wrap mt-4 gap-2 sm:mt-auto">
+                                    {project.tecnologies.map((tag, index) => (
+                                        <li className="bg-black/[0.7] px-3 py-1 text-[0.7rem] uppercase tracking-wider text-white rounded-full dark:text-white/70"
+                                            key={index}>
+                                            {tag}
+                                        </li>
+                                    ))}
+                                </ul>
+                                <p className="text-end mt-2 italic cursor-pointer">ver mais</p>
+                            </div>
 
-                        <div className="space-y-10 px-0 md:px-10 max-w-6xl">
-                            <h4 className="text-4xl font-semibold text-center">
-                                <span className="underline decoration-[#F7AB0A]/50">Nome do projeto {index + 1}:</span>{' '}
-                                okok
-                            </h4>
+                            <img
+                                src={project.imgPrincipal}
+                                alt="Project I worked on"
+                                className="absolute hidden sm:block top-8 -right-40 w-[28.25rem] rounded-t-lg shadow-2xl
+                                    transition 
+                                    group-hover:scale-[1.04]
+                                    group-hover:-translate-x-3
+                                    group-hover:translate-y-3
+                                    group-hover:-rotate-2
 
-                            <p className="text-lg text-center md:text-left">
-                                descricao
-                                descricaodescricaodescricaodescricaodescricaodescricao descricaodescricaodescricaodescricao
-                            </p>
-                        </div>
-                    </div>
+                                    group-even:group-hover:translate-x-3
+                                    group-even:group-hover:translate-y-3
+                                    group-even:group-hover:rotate-2
+
+                                    group-even:right-[initial] group-even:-left-40"
+                            />
+                        </section>
+                    </motion.div>
                 ))}
             </div>
 
