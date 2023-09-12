a2096959e0960db52f6e787d19f5197b6baffb75
vxfontes <nessa1vane@icloud.com>
2023-09-12 11:21:22 -0300

    fix: escondendo descrição para responsividade

--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -8,7 +8,7 @@ import Title from "./Title";
 
 const Experience = () => {
     return (
-        <div className="flex flex-col relative min-h-screen text-center md:text-left md:flex-row max-w-7xl px-10 justify-evenly mx-auto items-center bg-[#242424]">
+        <div className="flex flex-col relative min-h-screen text-center md:text-left md:flex-row max-w-screen-xl px-10 justify-evenly mx-auto items-end md:pb-12 bg-[#242424]">
             <Title>
                 {locale.experience.title}
             </Title>
@@ -38,7 +38,7 @@ const Experience = () => {
                         >
                             <h3 className="font-semibold capitalize vertical-timeline-element-title">{item.title}</h3>
                             <p className="font-normal !mt-0 vertical-timeline-element-subtitle">{item.location}</p>
-                            <p className="!mt-1 !font-normal text-gray-700 dark:text-white/75">
+                            <p className="!mt-1 !font-normal hidden 2xl:block text-gray-700 dark:text-white/75">
                                 {item.description}
                             </p>
                         </VerticalTimelineElement>
