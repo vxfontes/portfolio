24fac0890a1e8d6834b44e8e42aabb59b179b23b
Vanessa <nessa1vane@icloud.com>
2023-09-12 15:44:42 -0300

    fix: removendo dinamicidade

--- src/components/Skill.tsx
+++ src/components/Skill.tsx
@@ -1,23 +1,13 @@
-import { motion } from "framer-motion";
-
 type Props = {
-    directionLeft?: boolean;
     icon: string;
     name: string;
     percentage: string;
 }
 
-const Skill = ({ directionLeft, icon, name, percentage }: Props) => {
+const Skill = ({ icon, name, percentage }: Props) => {
     return (
         <div className="group relative flex cursor-pointer pb-6">
-            <motion.img
-                initial={{
-                    x: directionLeft ? -200 : 200,
-                    opacity: 0,
-                }}
-                transition={{ duration: 1 }}
-                whileInView={{ opacity: 1, x: 0 }}
-                className="rounded-full border border-gray-500 object-cover w-20 h-20 xl:w-32 xl:h-32 md:w-28 md:h-28 filter group-hover:grayscale transition duration-300 ease-in-out"
+            <img className="rounded-full border border-gray-500 object-cover w-20 h-20 xl:w-32 xl:h-32 md:w-28 md:h-28 filter group-hover:grayscale transition duration-300 ease-in-out"
                 src={icon}
             />
             <div className="absolute transition duration-300 group-hover:opacity-70 ease-in-out group-hover:bg-white w-20 h-20 md:w-28 md:h-28 xl:w-32 xl:h-32 rounded-full z-0">
--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -1,4 +1,3 @@
-import { motion } from "framer-motion";
 import Skill from "./Skill";
 import { skillsData } from "@/data/skillts";
 import { ptBR as locale } from "@/data/infos";
@@ -7,24 +6,19 @@ import Title from "./Title";
 
 const Skills = () => {
     return (
-        <motion.div
-            initial={{ opacity: 0 }}
-            whileInView={{ opacity: 1 }}
-            transition={{ duration: 1.5 }}
-            className="flex relative flex-col text-center md:text-left xl:flex-row max-w-[2000px] xl:px-10 min-h-screen justify-center xl:space-y-0 mt-4 mx-auto items-center"
-        >
+        <div className="flex relative flex-col text-center md:text-left xl:flex-row max-w-[2000px] xl:px-10 min-h-screen justify-center xl:space-y-0 mt-4 mx-auto items-center">
             <Title>
                 {locale.skill.title}
             </Title>
 
             <h3 className="absolute top-36 uppercase tracking-[3px] text-gray-500 text-sm">{locale.skill.sub}</h3>
 
-            <div className="mt-36 grid grid-cols-3 md:grid-cols-7 gap-5">
+            <div className="mt-36 grid grid-cols-4 md:grid-cols-7 gap-5">
                 {skillsData.map(skill => (
                     <Skill key={skill.name} icon={skill.icon} name={skill.name} percentage={skill.percentage} />
                 ))}
             </div>
-        </motion.div>
+        </div>
     );
 }
 
