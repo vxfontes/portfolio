894e6673fbf749923b98e5027fc2fda198900516
vxfontes <nessa1vane@icloud.com>
2023-09-12 11:21:39 -0300

    fix: mudando espaçamento em telas menores

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -4,7 +4,7 @@ interface Props {
 
 const Title = ({ children }: Props) => {
     return (
-        <h3 className="uppercase absolute top-24 tracking-[20px] text-gray-500 text-2xl">
+        <h3 className=" text-center justify-center uppercase absolute top-24 tracking-[10px] md:tracking-[20px] text-gray-500 text-2xl">
             {children}
         </h3>
     );
